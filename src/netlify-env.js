80f74aed0b000ea68bd86daaab346e50596b38c1
biemislam <bellomaryam@ymail.com>
2021-11-06 08:32:49 +0100

    specify environment variable on netlify

--- src/netlify-env.js
+++ src/netlify-env.js
@@ -1,6 +1,6 @@
 // import env from 'react-dotenv'
 
-const fs = require('fs')
-const path = `./.env`
-const envVar = `ENV_VAR_1=${process.env.REACT_APP_GEO_API_KEY}`
-fs.writeFileSync(path, envVar)
+// const fs = require('fs')
+// const path = './.env'
+// const envVar = `ENV_VAR_1=${process.env.REACT_APP_GEO_API_KEY}`
+// fs.writeFileSync(path, envVar)
